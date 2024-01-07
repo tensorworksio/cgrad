a8ece637ad3a7a6e34d7ee08206529e2dba5de28
denguir <anass.den@gmail.com>
2024-01-07 11:54:21 +0100

    test for div pow relu sum

--- tests/test_backops.c
+++ tests/test_backops.c
@@ -206,3 +206,180 @@ Test(mul, backward_mul_ft)
     tensor_free(y, true);
     tensor_free(a_grad, true);
 }
+
+Test(div, backward_div_tt)
+{   
+    log_set_level(LOG_INFO);
+
+    tensor_t* a = tensor((float[]){5., 6., 9., 8.}, (int[]){4}, 1, true);
+    tensor_t* b = tensor((float[]){1., 2., 3., 4.}, (int[]){4}, 1, true);
+    tensor_t* c = tensor_div(a, b);
+    tensor_t* y = tensor_sum(c);
+
+    tensor_backward(y);
+    
+    tensor_t* a_grad = tensor_create(a->shape, a->ndim, true);
+    tensor_set_data(a_grad, a->data, a->size);
+    tensor_set_grad(a_grad, (float[]){1., 0.5, 1./3, 0.25}, a->size);
+
+    tensor_t* b_grad = tensor_create(b->shape, b->ndim, true);
+    tensor_set_data(b_grad, b->data, b->size);
+    tensor_set_grad(b_grad, (float[]){-5., -1.5, -1., -0.5}, b->size);
+
+    cr_assert(tensor_equals(a, a_grad, true), "backward_div failed");
+    cr_assert(tensor_equals(b, b_grad, true), "backward_div failed");
+
+    tensor_free(y, true);
+    tensor_free(a_grad, true);
+    tensor_free(b_grad, true);
+}
+
+Test(div, backward_div_tf)
+{   
+    log_set_level(LOG_INFO);
+
+    tensor_t* a = tensor((float[]){5., 6., 9., 8.}, (int[]){4}, 1, true);
+
+    tensor_t* c = tensor_div_tf(a, 5.);
+    tensor_t* y = tensor_sum(c);
+
+    tensor_backward(y);
+
+    tensor_t* a_grad = tensor_create(a->shape, a->ndim, true);
+    tensor_set_data(a_grad, a->data, a->size);
+    tensor_set_grad(a_grad, (float[]){0.2, 0.2, 0.2, 0.2}, a->size);
+
+    cr_assert(tensor_equals(a, a_grad, true), "backward_div_tf failed");
+
+    tensor_free(y, true);
+    tensor_free(a_grad, true);
+}
+
+Test(div, backward_div_ft)
+{   
+    log_set_level(LOG_INFO);
+
+    tensor_t* a = tensor((float[]){5., 6., 9., 8.}, (int[]){4}, 1, true);
+
+    tensor_t* c = tensor_div_ft(5., a);
+    tensor_t* y = tensor_sum(c);
+
+    tensor_backward(y);
+    
+    tensor_t* a_grad = tensor_create(a->shape, a->ndim, true);
+    tensor_set_data(a_grad, a->data, a->size);
+    tensor_set_grad(a_grad, (float[]){-0.2, -0.1388889, -0.0617284, -0.078125}, a->size);
+
+    cr_assert(tensor_equals(a, a_grad, true), "backward_div_ft failed");
+
+    tensor_free(y, true);
+    tensor_free(a_grad, true);
+}
+
+Test(pow, backward_pow_tt)
+{   
+    log_set_level(LOG_INFO);
+
+    tensor_t* a = tensor((float[]){4., 3., 2., 1.}, (int[]){4}, 1, true);
+    tensor_t* b = tensor((float[]){1., 2., 3., 4.}, (int[]){4}, 1, true);
+    tensor_t* c = tensor_pow(a, b);
+    tensor_t* y = tensor_sum(c);
+
+    tensor_backward(y);
+
+    tensor_t* a_grad = tensor_create(a->shape, a->ndim, true);
+    tensor_set_data(a_grad, a->data, b->size);
+    tensor_set_grad(a_grad, (float[]){1., 6., 12., 4.}, a->size);
+
+    tensor_t* b_grad = tensor_create(b->shape, b->ndim, true);
+    tensor_set_data(b_grad, b->data, a->size);
+    tensor_set_grad(b_grad, (float[]){5.545177, 9.88751, 5.545177, 0.}, b->size);
+
+    cr_assert(tensor_equals(a, a_grad, true), "backward_pow failed");
+    cr_assert(tensor_equals(b, b_grad, true), "backward_pow failed");
+
+    tensor_free(y, true);
+    tensor_free(a_grad, true);
+    tensor_free(b_grad, true);
+}
+
+Test (pow, backward_pow_tf)
+{   
+    log_set_level(LOG_INFO);
+
+    tensor_t* a = tensor((float[]){4., 3., 2., 1.}, (int[]){4}, 1, true);
+
+    tensor_t* c = tensor_pow_tf(a, 5.);
+    tensor_t* y = tensor_sum(c);
+
+    tensor_backward(y);
+
+    tensor_t* a_grad = tensor_create(a->shape, a->ndim, true);
+    tensor_set_data(a_grad, a->data, a->size);
+    tensor_set_grad(a_grad, (float[]){1280.,  405., 80., 5.}, a->size);
+
+    cr_assert(tensor_equals(a, a_grad, true), "backward_pow_tf failed");
+
+    tensor_free(y, true);
+    tensor_free(a_grad, true);
+}
+
+Test(pow, backward_pow_ft)
+{   
+    log_set_level(LOG_INFO);
+
+    tensor_t* a = tensor((float[]){4., 3., 2., 1.}, (int[]){4}, 1, true);
+
+    tensor_t* c = tensor_pow_ft(5., a);
+    tensor_t* y = tensor_sum(c);
+
+    tensor_backward(y);
+
+    tensor_t* a_grad = tensor_create(a->shape, a->ndim, true);
+    tensor_set_data(a_grad, a->data, a->size);
+    tensor_set_grad(a_grad, (float[]){1005.898743, 201.179749, 40.235947, 8.047190}, a->size);
+
+    cr_assert(tensor_equals(a, a_grad, true), "backward_pow_ft failed");
+
+    tensor_free(y, true);
+    tensor_free(a_grad, true);
+}
+
+Test(relu, backward_relu) 
+{
+    log_set_level(LOG_INFO);
+
+    tensor_t* a = tensor((float[]){-1., 2., -3., 4.}, (int[]){4}, 1, true);
+    tensor_t* c = tensor_relu(a);
+    tensor_t* y = tensor_sum(c);
+
+    tensor_backward(y);
+
+    tensor_t* a_grad = tensor_create(a->shape, a->ndim, true);
+    tensor_set_data(a_grad, a->data, a->size);
+    tensor_set_grad(a_grad, (float[]){0., 1., 0., 1.}, a->size);
+
+    cr_assert(tensor_equals(a, a_grad, true), "backward_relu failed");
+
+    tensor_free(y, true);
+    tensor_free(a_grad, true);
+}
+
+Test(sum, backward_sum)
+{   
+    log_set_level(LOG_INFO);
+
+    tensor_t* a = tensor((float[]){-1., 2., -3., 4.}, (int[]){4}, 1, true);
+    tensor_t* y = tensor_sum(a);
+
+    tensor_backward(y);
+
+    tensor_t* a_grad = tensor_create(a->shape, a->ndim, true);
+    tensor_set_data(a_grad, a->data, a->size);
+    tensor_set_grad(a_grad, (float[]){1., 1., 1., 1.}, a->size);
+
+    cr_assert(tensor_equals(a, a_grad, true), "backward_sum failed");
+
+    tensor_free(y, true);
+    tensor_free(a_grad, true);
+}
